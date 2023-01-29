a71f4764d18ecd6eb931daaf1393aa941e88ca2e
navin chhorn <navin.chhorn09012004@gmail.com>
2023-01-29 15:25:05 +0700

    [CON-34]: Shows detail on controller page

--- models/detail.model.php
+++ models/detail.model.php
@@ -1,10 +1,10 @@
 
 <?php
 
-function getUserData() : array
+function getShowData() : array
 {
    global $connection;
-   $statement = $connection->prepare("select * from shows");
+   $statement = $connection->prepare("select * from movies");
    $statement->execute();
    return $statement->fetchAll();
 }
