572e93f040fca7363fd4476f26f12050376b91e2
makara-12 <makara.phoem@student.passerellesnumeriques.org>
2023-02-06 16:40:21 +0700

    [CON-73]: Create form new show

--- router.php
+++ router.php
@@ -6,6 +6,7 @@ $routes = [
     '/register' => 'controllers/forms/register/form.register.controller.php',
     '/login' => 'controllers/forms/login/form.login.controller.php',
     '/detail' => 'views/detail/detail.view.php',
+    '/show' => 'views/forms/form.create.show.view.php',
 
 ];
 
