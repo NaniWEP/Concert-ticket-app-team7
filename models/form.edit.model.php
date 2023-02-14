f3d6ca98d6da655e4ca694f32bd9b28057bb7b40
makara-12 <makara.phoem@student.passerellesnumeriques.org>
2023-02-14 20:34:19 +0700

    [CON-76]: Keep data input when refresh page

--- models/form.edit.model.php
+++ models/form.edit.model.php
@@ -1,6 +1,5 @@
 <?php
 
-
 function getshowbyId($id) : array
 {
     global $connection;
@@ -12,12 +11,12 @@ function getshowbyId($id) : array
 function getVenuebyId($id) : array
 {
     global $connection;
-    $statement = $connection->prepare("SELECT venues.name,venues.address,venues.id FROM detail right join venues on venues.id = detail.venue_id where show_id=$id;");
+    $statement = $connection->prepare("SELECT venues.name, venues.address, venues.id FROM detail right join venues on venues.id = detail.venue_id where show_id=$id;");
     $statement->execute();
     return $statement->fetch(PDO::FETCH_ASSOC);
 }
 
-function editShow(int $id,string $title, string $description, int $type_id, int $running_time, string $language, string $subtitle, string $img, string  $trailer) :bool{
+function editShow(int $id, string $title, string $description, int $type_id, int $running_time, string $language, string $subtitle, string $img, string  $trailer) :bool{
     global $connection;
     echo $subtitle;
     $statement = $connection->prepare("update shows set title = :title ,img = :img,description=:description,type_id=:type_id,running_time=:running_time,trailer=:trailer,language=:language,subtitle=:subtitle where id= :id ");
